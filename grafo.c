eaab2aa1b1c7a2c5cdd556b0e7eba406999f67d7
Mateus Victor <mateusvictorct@gmail.com>
2024-04-18 09:07:36 -0300

    add a questão 2

--- grafo.c
+++ grafo.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 
 #define tam 3771
+// questão 2
+int matrizadj[tam][tam];
+int gVertice( int vertice, int n){
+    int i, grauVert = 0;
+    for (i = 0; i < n; i++) {
+        if (matrizadj[vertice][i]) {
+            grauVert++;
+        }
+    }
+    return grauVert;
+}
+// fim da funcção da questão 2
 int main(){
 int n,m, i,j;
 int *grauVertice;
@@ -22,10 +34,18 @@ int a, maiorGrau = 0,verticeMaxGrau;
             verticeMaxGrau = i;
         }
     }
-
     printf("O vertice com o maior grau e o vertice %d com grau %d\n", verticeMaxGrau, maiorGrau);
 
     free(grauVertice);
-    
+
+    // Quesão 2
+    int x;
+    FILE *arquivo = fopen("dados_grafos_graus.txt","w");
+    for(i=0;i<tam;i++){
+        int grauVert = gVertice(i,x);
+         fprintf(arquivo, "%d %d\n", i, grauVert);
+    }
+    fclose(arquivo);
+    // fim da questao 2
     return 0;
 }
